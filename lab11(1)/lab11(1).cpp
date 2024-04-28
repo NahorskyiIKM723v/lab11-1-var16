8b9718a0f4bebe6c06653395a8c608a1d6d98df7
igor <igor@DESKTOP-EH4IM96>
2024-04-28 16:59:31 +0300

    Добавьте файлы проекта.

---
+++ lab11(1)/lab11(1).cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <Windows.h>
+using namespace std;
+
+int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    int a, b, c;
+    cout << "Введіть значення змінної a: ";
+    cin >> a;
+    cout << "Введіть значення змінної b: ";
+    cin >> b;
+    cout << "Введіть значення змінної c: ";
+    cin >> c;
+
+    int* ptr_a = &a;
+    int* ptr_b = &b;
+    int* ptr_c = &c;
+
+    int temp = *ptr_a;
+    *ptr_a = *ptr_b;
+    *ptr_b = temp;
+
+    temp = *ptr_a;
+    *ptr_a = *ptr_c;
+    *ptr_c = temp;
+
+    cout << "Результат обміну:\n";
+    cout << "a = " << a << endl;
+    cout << "b = " << b << endl;
+    cout << "c = " << c << endl;
+
+    return 0;
+}
